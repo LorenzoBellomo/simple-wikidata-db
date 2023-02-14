b40f0ef641d179cc42991630ef7d305e2ce8ee17
LorenzoBellomo <lorenzo.bellomo.lb@gmail.com>
2023-02-14 17:57:11 +0100

    filter textual redirects

--- simple_wikidata_db/preprocess_utils/worker_process_bio.py
+++ simple_wikidata_db/preprocess_utils/worker_process_bio.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from multiprocessing import Queue
+import re
 
 # properties which encode some alias/name
 import ujson
@@ -13,6 +14,7 @@ ALIAS_PROPERTIES = {'P138', 'P734', 'P735', 'P742', 'P1448', 'P1449', 'P1477', '
 IGNORE = {'wikibase-lexeme', 'musical-notation', 'globe-coordinate', 'commonsMedia', 'geo-shape', 'wikibase-sense',
           'wikibase-property', 'math', 'tabular-data'}
 
+reg = re.compile(r"[^A-Za-z ]+")
 
 with open("../bio_mapping.json", "r") as json_file:
     CAT_MAPPING = ujson.load(json_file)
@@ -106,10 +108,11 @@ def process_json(obj, language_id="en"):
     # extract aliases
     if language_id in obj['aliases']:
         for alias in obj['aliases'][language_id]:
-            out_data['aliases'].append({
-                'qid': id,
-                'alias': alias['value'],
-            })
+            if reg.findall(alias):
+                out_data['aliases'].append({
+                    'qid': id,
+                    'alias': alias['value'],
+                })
 
     # extract english wikipedia sitelink -- we just add this to the external links table
     if f'{language_id}wiki' in obj['sitelinks']:
